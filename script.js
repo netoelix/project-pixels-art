d14c1e7b42420760a794dca72d89838907d58348
Elix Merlino Neto <neto.elix@outlook.com>
2023-07-19 04:35:10 -0300

    Criando titulo e paleta de cores na pagina

---
+++ script.js
@@ -0,0 +1,6 @@
+const colorPalet = document.querySelectorAll('.color');
+
+colorPalet[0].style.backgroundColor = 'blue';
+colorPalet[1].style.backgroundColor = 'red';
+colorPalet[2].style.backgroundColor = 'green';
+colorPalet[3].style.backgroundColor = 'yellow';
