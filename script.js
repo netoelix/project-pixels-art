e48ca3c5db6d3de788bbb0a225dbbb0f9273a911
Elix Merlino Neto <neto.elix@outlook.com>
2023-07-19 05:11:36 -0300

    Criando campo de pixels

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ colorPalet[0].style.backgroundColor = 'blue';
 colorPalet[1].style.backgroundColor = 'red';
 colorPalet[2].style.backgroundColor = 'green';
 colorPalet[3].style.backgroundColor = 'yellow';
+
+const creatPixel = () => {
+  for (let index = 0; index < 25; index += 1) {
+    const pixelPainel = document.getElementById('pixel-board');
+    const newElementPixel = document.createElement('div');
+    newElementPixel.className = 'pixel';
+    pixelPainel.appendChild(newElementPixel);
+  }
+};
+creatPixel();
